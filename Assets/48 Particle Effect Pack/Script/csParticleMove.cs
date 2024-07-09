55a5998fe88f6c2851d074a27bc73cb872d20182
edisonpig <potsunchu@gmail.com>
2024-07-09 02:23:43 +0100

    Adding vfx assets

---
+++ Assets/48 Particle Effect Pack/Script/csParticleMove.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class csParticleMove : MonoBehaviour
+{
+    public float speed = 0.1f;
+    bool collision;
+
+    void Update()
+    {
+
+        RaycastHit hit;
+
+        if (Physics.SphereCast(transform.position, transform.lossyScale.x / 2, transform.forward, out hit, 0.5f + speed * Time.deltaTime))
+        {
+            collision = true;
+            Debug.Log("touched");
+            Destroy(gameObject, 1f);
+        }
+        else collision = false;
+        if (!collision)
+        {
+            transform.Translate(Vector3.back * speed);
+        }
+
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        Debug.Log(gameObject.name + " touched");
+    }
+}
